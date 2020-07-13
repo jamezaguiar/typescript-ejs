3b5550220566ac7804647649afe8c77425f693ba
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-07-13 14:01:02 -0300

    Login page finalized

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(express.static(resolve(__dirname, '..', 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', resolve(__dirname, 'views'));
